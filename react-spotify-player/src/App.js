99890dd0b20f75517296ec6487ab9aeec940a853
eled47 <ele2117@columbia.edu>
2020-06-17 16:03:30 -0400

    Initializing Spotify Player

---
+++ react-spotify-player/src/App.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import * as $ from "jquery";
+import hash from "./hash";
+import Player from "./Player";
+import logo from './logo.svg';
+import './App.css';
+
+export const authEndpoint = 'https://accounts.spotify.com/authorize';
+
+// Replace with your app's client ID, redirect URI and desired scopes
+const clientId = "8757fbf98c8a4aa190b842ddbaa50e70";
+const redirectUri = "http://localhost:3000";
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-playback-state",
+];
+
+// Get the hash of the url
+const hash = window.location.hash
+  .substring(1)
+  .split("&")
+  .reduce(function(initial, item) {
+    if (item) {
+      var parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+    }
+    return initial;
+  }, {});
+
+window.location.hash = "";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      token: null,
+      item: {
+        album: {
+          images: [{ url: "" }]
+        },
+        name: "",
+        artists: [{ name: "" }],
+        duration_ms:0,
+      },
+      is_playing: "Paused",
+      progress_ms: 0
+    };
+
+    this.getCurrentlyPlaying = this.getCurrentlyPlaying.bind(this);
+  }
+
+  componentDidMount() {
+    // Set token
+    let _token = hash.access_token;
+    if (_token) {
+      // Set token
+      this.setState({
+        token: _token
+      });
+    }
+  }
+
+render() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      {!this.state.token && (
+        <a
+          className="btn btn--loginApp-link"
+          href={`${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}
+        >
+          Login to Spotify
+        </a>
+      )}
+      {this.state.token && (
+        // Spotify Player Will Go Here In the Next Step
+      )}
+      </header>
+    </div>
+  );
+  }
+}
+
+export default App;
